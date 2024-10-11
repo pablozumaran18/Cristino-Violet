6faf2ff00adfcfc450f39581a993d0f4a873dfb4
Zumvrvn <zumaranpablo18@gmial.com>
2024-10-11 15:40:16 -0300

    Actualizacion 11-10-2024

--- Sistema/carrito/models.py
+++ Sistema/carrito/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from Productos.models import Producto
+
+class Carrito(models.Model);
+    producto_id = models.ForeignKey(Producto,verbose_name="¨Producto ID", on_delete=models.CASCADE)
 
 # Create your models here.
